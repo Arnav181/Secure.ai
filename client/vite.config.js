d82fed246907ed3758e6d7d387d171d6c01721fb
Arnav181 <158609672+Arnav181@users.noreply.github.com>
2025-06-21 09:07:12 +0530

    package and vite changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      '9e9d-2406-7488-5d34-ad69-5c3d-e7e3-5007.ngrok-free.app',
+      'c18f-2406-7400-54-2340-a0d9-5e34-e7e3-5807.ngrok-free.app',
+      // You can add more hosts here if needed, e.g., 'localhost', '127.0.0.1'
+    ],
+  },
 })
