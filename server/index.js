b1fe47ecd02812a408e7485682a87ba91fb47f22
Ayush201738982361 <ayushnair87@gmail.com>
2025-06-24 00:06:38 +0530

    Added cors middleware for sending the data to client side

--- server/index.js
+++ server/index.js
@@ -4,9 +4,17 @@ require("dotenv").config();
 const userRoutes = require("./routes/userRoutes");
 const PORT = process.env.PORT;
 const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 connectDatabase(MONGO_CONNECTION_STRING).then(() => {
   console.log("MongoDB Connected");
