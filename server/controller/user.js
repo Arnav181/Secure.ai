59557cc1a8f57b2ce103fc5b308400658947b83a
Ayush201738982361 <ayushnair87@gmail.com>
2025-06-27 10:50:09 +0530

    Used bcrypt to hash user password using salt

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,4 +1,6 @@
 const user = require("../model/user");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const createNewUser = async (req, res) => {
   const { username, email, password } = req.body;
@@ -10,7 +12,13 @@ const createNewUser = async (req, res) => {
         .status(409)
         .json({ msg: "User already exits with this email" });
     }
-    const userdetails = await user.create({ username, email, password });
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const userdetails = await user.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
     res
       .status(201)
       .json({ msg: "User created successfully", createdUser: userdetails });
