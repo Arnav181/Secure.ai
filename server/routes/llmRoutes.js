4363ec704f79cbad6f717134814993e2602c59c8
Ayush201738982361 <ayushnair87@gmail.com>
2025-07-24 22:18:16 +0530

    Added new route for chat

--- server/routes/llmRoutes.js
+++ server/routes/llmRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const { analyzeZip } = require("../controller/llm");
+const { analyzeZip, chatMessage } = require("../controller/llm");
 
 const upload = multer({ storage: multer.memoryStorage() });
 router.post("/codebase", upload.single("zipfile"), analyzeZip);
+router.post("/message", chatMessage);
 
 module.exports = router;
