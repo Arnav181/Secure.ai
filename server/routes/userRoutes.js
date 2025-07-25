7c92493a416d2d5c5243033c9d2b352bc75c96a8
Ayush201738982361 <ayushnair87@gmail.com>
2025-07-26 00:20:41 +0530

    Added checkForAuth middleware to the routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { createNewUser, loginUser } = require("../controller/user");
+const { checkForAuth } = require("../middleware/auth");
 const router = express.Router();
 
+router.use(checkForAuth);
 router.post("/signup", createNewUser);
 router.post("/login", loginUser);
 
