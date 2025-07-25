941934467ffaef63d770df749769892367c95d8e
Ayush201738982361 <ayushnair87@gmail.com>
2025-07-26 00:18:32 +0530

    Added a middleware to validate user based on cookie token

---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+const { getUser } = require("../services/auth");
+
+const checkForAuth = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ msg: "No cookie found" });
+  }
+  try {
+    const user = getUser(token);
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log("Error in validating user", err);
+  }
+};
+
+module.exports = { checkForAuth };
