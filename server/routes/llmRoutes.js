f250454ea5e6be014a3316a1c240034622d7f955
Ayush201738982361 <ayushnair87@gmail.com>
2025-07-27 00:03:20 +0530

    Added a new backend route for application chatbot feature

--- server/routes/llmRoutes.js
+++ server/routes/llmRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const { analyzeZip, chatMessage } = require("../controller/llm");
+const {
+  analyzeZip,
+  chatMessage,
+  chatBotMessage,
+} = require("../controller/llm");
 
 const upload = multer({ storage: multer.memoryStorage() });
 router.post("/codebase", upload.single("zipfile"), analyzeZip);
 router.post("/message", chatMessage);
+router.post("/chatbot", chatBotMessage);
 
 module.exports = router;
