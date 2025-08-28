8879b26785d30d4295553731483c8e2e9ef0021b
Ayush201738982361 <ayushnair87@gmail.com>
2025-08-28 22:59:59 +0530

    Routes for logout and user-details

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const { createNewUser, loginUser } = require("../controller/user");
+const {
+  createNewUser,
+  loginUser,
+  logoutUser,
+  sendUserInfo,
+} = require("../controller/user");
 const { checkForAuth } = require("../middleware/auth");
 const router = express.Router();
 
 router.use(checkForAuth);
 router.post("/signup", createNewUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/userdetails", sendUserInfo);
 
 module.exports = router;
